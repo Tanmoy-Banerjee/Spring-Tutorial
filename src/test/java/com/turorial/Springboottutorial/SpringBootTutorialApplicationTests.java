731a97abe89d643cced63c0a99c73b04e594d380
Tanmoy-Banerjee <tanmoybanerjeetvf@gmail.com>
2023-04-21 01:52:36 +0530

    Adding first Tutorial commit

---
+++ src/test/java/com/turorial/Springboottutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turorial.Springboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
